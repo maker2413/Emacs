af186a726f2594f9249814de36837802b705a607
maker2413 <maker2413@gmail.com>
2021-01-03 13:31:36 -0800

    added hotkey for emacs and updated gitignore

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(package-selected-packages '(yaml-mode use-package lua-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
