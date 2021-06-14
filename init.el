6f548f17c9d02aead708af7739c28e5f5f7106fa
maker2413 <maker2413@gmail.com>
2021-06-14 00:04:20 -0500

    re themed emacs and trying out helper tools

--- init.el
+++ init.el
@@ -1,22 +1,92 @@
-(add-to-list 'load-path "~/.config/emacs/elisp")
+(setq inhibit-startup-message t)
 
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+(menu-bar-mode -1)
+
+(setq visible-bell t)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 120)
+
+;; Initialize package sources
 (require 'package)
+
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; Run packages file
-(require 'init-packages)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Run my config file
-(require 'init-config)
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package afternoon-theme)
+
+;;(load-theme 'afternoon t)
+
+(use-package diminish)
+
+(use-package counsel
+  :bind(("M-x" . counsel-M-x)))
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper))
+  :config
+  (ivy-mode 1))
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode t)
+  :custom ((doom-modeline-height 10)))
+
+(use-package doom-themes
+  :init (load-theme 'doom-palenight t))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(eshell-mode-hook
+		shell-mode-hook
+		term-mode-hook))
+  (add-hook mode(lambda() (display-line-numbers-mode 0))))
+
+;; (use-package rainbow-delimiters
+;;   :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode t)
+  :diminish
+  :config
+  (setq which-key-idls-delay 0.5))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(package-selected-packages '(yaml-mode use-package lua-mode)))
+ '(package-selected-packages
+   '(helpful ivy-rich which-key doom-themes counsel yaml-mode use-package terraform-mode markdown-mode lua-mode json-mode ivy groovy-mode go-mode fish-mode exec-path-from-shell doom-modeline dockerfile-mode diminish afternoon-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
