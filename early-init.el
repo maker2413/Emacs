5d4610d77d03cd125ddd4dbc8a69247650663214
maker2413 <maker2413@gmail.com>
2025-10-26 00:27:23 -0500

    updating early init script

--- early-init.el
+++ early-init.el
@@ -4,4 +4,11 @@
 ;; Only apply on GNOME to avoid affecting other systems
 (when (and (getenv "XDG_CURRENT_DESKTOP")
            (string-match-p "GNOME" (getenv "XDG_CURRENT_DESKTOP")))
-  (add-to-list 'default-frame-alist '(fullscreen . maximized)))
+  (add-to-list 'default-frame-alist '(fullscreen . maximized))
+  (if (display-graphic-p)
+      (scroll-bar-mode -1))
+  (tool-bar-mode -1)
+  (tooltip-mode -1)
+  (require 'fringe)
+  (set-fringe-mode 10)
+  (menu-bar-mode -1))
